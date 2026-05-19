a989fdcfc84d518ecfa90f0ed5d7173a6063bc6f
MaxencePeq <maxence.pequeno@icloud.com>
2026-05-19 18:04:38 +0200

    Feat : Ajout d'un param de bloquage de hauteur des images

--- portfolio/src/component/lightbox.tsx
+++ portfolio/src/component/lightbox.tsx
@@ -1,16 +1,25 @@
 type LightboxProps = {
   darkmode: boolean;
   image: React.ReactNode;
+  ImageBlockingHeight: boolean;
 };
 
-export default function Lightbox({ darkmode, image }: LightboxProps) {
+export default function Lightbox({
+  darkmode,
+  image,
+  ImageBlockingHeight,
+}: LightboxProps) {
   const bgColor = darkmode ? "bg-[#3A3A5A]" : "bg-white";
   const borderColor = darkmode ? "border-gray-600" : "border-white";
   const hover = "hover:-translate-y-[2px] transition-transform duration-200";
 
+  let h;
+  if (ImageBlockingHeight) {
+    h = "h-32";
+  }
   return (
     <div
-      className={`${bgColor} w-full h-32 p-3 border ${borderColor} rounded-xl flex justify-center items-center overflow-hidden ${hover}`}
+      className={`${bgColor} w-full ${h} p-3 border ${borderColor} rounded-xl flex justify-center items-center overflow-hidden ${hover}`}
     >
       {image}
     </div>
